98189459ac3496a6a65989105afe8c38a2845803
Jorge Nathanael Isaac Reyes Celpa     (LATAM) <jorgenathanael.reyes@latam.com>
2023-09-24 21:34:32 -0300

    prueba

--- tests/utils.py
+++
@@ -1,9 +0,0 @@
-from jsonschema import validate, exceptions
-
-def check_valid_schema(schema, data):
-  try:
-    validate(data, schema)
-    return True
-  except exceptions.ValidationError as error:
-    print(f"\n\n\033[91mValidation Error: {error.message}")
-    return False
